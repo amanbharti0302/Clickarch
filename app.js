9380fc51b2c4f2aa21d27cc4b3c61ad4502f6b40
AMAN BHARTI <amanbharti0302@gmail.com>
2020-08-24 23:23:01 +0530

    basic setup

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.use('/',userrouter);
 
 app.all('*', (req, res, next) => {
-  res.send('<html><head><title>500 Internal Server Error</title></head><body bgcolor="white"><center><h1>500 Internal Server Error</h1></center><hr><center>covid19pr.com <br>website in development mode plase contact :7250720774 (aman bharti)</center></body></html>');
+  res.send('<html><head><title>500 Internal Server Error</title></head><body bgcolor="white"><center><h1>500 Internal Server Error</h1></center><hr><center>National institute of technology Patna <br>website in development mode plase contact :7250720774 (aman bharti)</center></body></html>');
 });
 
 module.exports = app;
